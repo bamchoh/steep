113664fdc3f5ad57ae85366e06c7780a6752b02f
Yoshihiko Yamanaka <bamchoh@gmail.com>
2021-09-04 16:26:37 +0900

    Call popen2 with new_pgroup parameter instead of pgroup in case of windows

--- lib/steep/server/worker_process.rb
+++ lib/steep/server/worker_process.rb
@@ -33,7 +33,11 @@ module Steep
           command << "--delay-shutdown"
         end
 
-        stdin, stdout, thread = Open3.popen2(*command, pgroup: true)
+        stdin, stdout, thread = if /mingw|mswin/ =~ RUBY_PLATFORM
+                                  Open3.popen2(*command, new_pgroup: true)
+                                else
+                                  Open3.popen2(*command, pgroup: true)
+                                end
         stderr = nil
 
         writer = LanguageServer::Protocol::Transport::Io::Writer.new(stdin)
